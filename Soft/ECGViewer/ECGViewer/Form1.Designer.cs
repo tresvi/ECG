00d76d52fe46c10c180996dda03ec501e6f6454f
Raul Palavecino <raulpalav@gmail.com>
2024-09-09 01:25:55 -0300

    Se agrega programa Net 4.8

---
+++ Soft/ECGViewer/ECGViewer/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ECGViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartEspectro = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnResetZoom = new System.Windows.Forms.Button();
+            this.BtnMarcadores = new System.Windows.Forms.Button();
+            this.BtnLineaReferencia = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartEspectro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartEspectro
+            // 
+            this.chartEspectro.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chartEspectro.BorderlineColor = System.Drawing.Color.Black;
+            chartArea1.AxisX.LabelAutoFitMaxFontSize = 15;
+            chartArea1.AxisX.LabelAutoFitMinFontSize = 8;
+            chartArea1.AxisX.Title = "Tiempo[Seg]";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisY.Maximum = 250D;
+            chartArea1.AxisY.Title = "Amplitud";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            chartArea1.Name = "ChartArea1";
+            this.chartEspectro.ChartAreas.Add(chartArea1);
+            this.chartEspectro.Location = new System.Drawing.Point(58, 95);
+            this.chartEspectro.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.chartEspectro.Name = "chartEspectro";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.MarkerStep = 10;
+            series1.Name = "Muestras";
+            this.chartEspectro.Series.Add(series1);
+            this.chartEspectro.Size = new System.Drawing.Size(1869, 629);
+            this.chartEspectro.TabIndex = 1;
+            this.chartEspectro.Text = "chart1";
+            this.chartEspectro.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chartEspectro_MouseClick);
+            this.chartEspectro.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chartEspectro_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(92, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnResetZoom
+            // 
+            this.BtnResetZoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnResetZoom.Location = new System.Drawing.Point(416, 18);
+            this.BtnResetZoom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnResetZoom.Name = "BtnResetZoom";
+            this.BtnResetZoom.Size = new System.Drawing.Size(261, 58);
+            this.BtnResetZoom.TabIndex = 3;
+            this.BtnResetZoom.Text = "Reset Zoom";
+            this.BtnResetZoom.UseVisualStyleBackColor = true;
+            this.BtnResetZoom.Click += new System.EventHandler(this.BtnResetZoom_Click);
+            // 
+            // BtnMarcadores
+            // 
+            this.BtnMarcadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMarcadores.Location = new System.Drawing.Point(746, 18);
+            this.BtnMarcadores.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnMarcadores.Name = "BtnMarcadores";
+            this.BtnMarcadores.Size = new System.Drawing.Size(261, 58);
+            this.BtnMarcadores.TabIndex = 4;
+            this.BtnMarcadores.Text = "Marcadores";
+            this.BtnMarcadores.UseVisualStyleBackColor = true;
+            this.BtnMarcadores.Click += new System.EventHandler(this.BtnMarcadores_Click);
+            // 
+            // BtnLineaReferencia
+            // 
+            this.BtnLineaReferencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLineaReferencia.Location = new System.Drawing.Point(1015, 18);
+            this.BtnLineaReferencia.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnLineaReferencia.Name = "BtnLineaReferencia";
+            this.BtnLineaReferencia.Size = new System.Drawing.Size(261, 58);
+            this.BtnLineaReferencia.TabIndex = 5;
+            this.BtnLineaReferencia.Text = "Linea de referencia";
+            this.BtnLineaReferencia.UseVisualStyleBackColor = true;
+            this.BtnLineaReferencia.Click += new System.EventHandler(this.BtnLineaReferencia_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1284, 5);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(261, 85);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "2da Serie con DataPoints";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1579, 5);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(261, 85);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "V.Anott con lineas";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1666, 65);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(261, 85);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "V.Anott con lineas 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1960, 777);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BtnLineaReferencia);
+            this.Controls.Add(this.BtnMarcadores);
+            this.Controls.Add(this.BtnResetZoom);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chartEspectro);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartEspectro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartEspectro;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnResetZoom;
+        private System.Windows.Forms.Button BtnMarcadores;
+        private System.Windows.Forms.Button BtnLineaReferencia;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Soft/ECGViewer/ECGViewer/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+//TODO: Agregar captura en vivo desde puerto serie
+//TODO: Agregar sistema de marcas
+//TODO: Agregar guardado de archivo
+//TODO: Agregar exportacion a CSV y tabla C++
+
+//https://stackoverflow.com/questions/25801257/c-sharp-line-chart-how-to-create-vertical-line
+
+namespace ECGViewer
+{
+    public partial class Form1 : Form
+    {
+        Series _graphSerie;
+        private Panel chartPanel;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configura el gráfico
+            chartEspectro.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;  // Mostrar barra de desplazamiento
+            chartEspectro.ChartAreas[0].AxisX.ScrollBar.Size = 15;                  // Tamaño de la barra de desplazamiento
+
+            // Define el tamaño de la vista y habilita el desplazamiento
+            chartEspectro.ChartAreas[0].AxisX.ScaleView.Zoomable = true;             // Habilitar zoom
+            chartEspectro.ChartAreas[0].AxisX.ScaleView.Size = 50;                   // Cantidad de puntos visibles inicialmente
+
+            // Habilitar el desplazamiento
+            chartEspectro.ChartAreas[0].AxisX.ScaleView.MinSize = 1;                 // Mínima vista permitida (1 punto)
+            chartEspectro.ChartAreas[0].AxisX.ScaleView.Position = 0;                // Posición inicial de la vista
+
+            // Configura el comportamiento del scroll
+            chartEspectro.ChartAreas[0].AxisX.ScaleView.SmallScrollSize = 5;         // Tamaño del desplazamiento pequeño (5 puntos)
+            chartEspectro.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = 1;      // Tamaño mínimo de desplazamiento
+
+            // Opcional: Configurar los intervalos del eje X para hacer la navegación más clara
+            chartEspectro.ChartAreas[0].AxisX.Interval = 1;
+
+            ActivarZoom(chartEspectro, true);
+
+
+
+
+        }
+
+        private void Graficar()
+        {
+
+
+            //Creo la nueva serie de datos.
+            _graphSerie = new Series("Muestras");
+            _graphSerie.Color = System.Drawing.Color.Green;
+            _graphSerie.ChartType = SeriesChartType.Line;  //SeriesChartType.Column; //SeriesChartType.Line;
+            _graphSerie.BorderWidth = 1; //2;
+            _graphSerie.XValueType = ChartValueType.Single;
+            _graphSerie.YValueType = ChartValueType.Single;
+            //chartEspectro.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            //chartEspectro.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+
+            ChartArea chartArea = chartEspectro.ChartAreas[0];
+
+            // Configura los valores mínimo y máximo del eje Y
+            chartArea.AxisY.Minimum = -0.12;
+            chartArea.AxisY.Maximum = 0.5;
+
+            string filePath = @"..\..\CSV\ECG_20Seg_Filtrado.txt"; // Ruta al archivo CSV
+           // List<(double, double)> muestras = LoadCsvData(filePath);
+
+
+            List<DataPoint> listaMaximos = null;
+            List<DataPoint> curva;
+            List<(float, float)> dataList = LoadCsvData(filePath);
+            Series series = chartEspectro.Series["Muestras"];
+
+            foreach (var (x, y) in dataList)
+            {
+                series.Points.AddXY(x, -1*y);
+            }
+            _graphSerie = series;
+
+            //int counter = 0;
+            //foreach ((double, double) muestra in muestras)
+            //{
+            //    counter++;
+            //    DataPoint point = new DataPoint((double)counter, 1000 * muestra.Item2);
+            //    _graphSerie.Points.Add(point);
+            //}
+
+
+            //Visuaizacion del eje X
+            // Configurar el intervalo del eje X
+            chartEspectro.ChartAreas[0].AxisX.Interval = 1;  // Intervalo de 1 unidad
+
+            // Configurar el formato de las etiquetas del eje X
+            chartEspectro.ChartAreas[0].AxisX.LabelStyle.Format = "0.00";  // Mostrar solo números enteros (sin decimales)
+
+            // Opcional: Asegurarse de que las etiquetas estén alineadas correctamente
+            // chartEspectro.ChartAreas[0].AxisX.RoundAxisValues();
+
+
+            //Autoescala
+            // Habilitar autoescala en el eje X
+            chartEspectro.ChartAreas[0].AxisX.Minimum = Double.NaN;  // Autoajustar el mínimo
+            chartEspectro.ChartAreas[0].AxisX.Maximum = Double.NaN;  // Autoajustar el máximo
+
+            // Habilitar autoescala en el eje Y
+            chartEspectro.ChartAreas[0].AxisY.Minimum = Double.NaN;  // Autoajustar el mínimo
+            chartEspectro.ChartAreas[0].AxisY.Maximum = Double.NaN;  // Autoajustar el máximo
+        }
+
+
+        static List<(float, float)> LoadCsvData(string filePath)
+        {
+            var dataList = new List<(float, float)>();
+
+            // Verifica si el archivo existe
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("El archivo CSV no se encontró.", filePath);
+            }
+
+            // Lee el archivo línea por línea
+            foreach (var line in File.ReadLines(filePath))
+            {
+                // Divide la línea en partes usando la coma como delimitador
+                var parts = line.Split('\t');
+
+                if (parts.Length >= 2)
+                {
+                    // Convierte las partes a float y añade a la lista
+                    if (float.TryParse(parts[0], NumberStyles.Float, CultureInfo.InvariantCulture, out float value1) &&
+                        float.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out float value2))
+                    {
+                        dataList.Add((value1, value2));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No se pudieron convertir los valores: {line}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Línea con formato incorrecto: {line}");
+                }
+            }
+
+            return dataList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graficar();
+        }
+
+
+        public static void ActivarZoom(Chart chart, bool enable)
+        {
+            chart.ChartAreas[0].CursorX.IsUserEnabled = enable;
+            chart.ChartAreas[0].CursorX.IsUserSelectionEnabled = enable;
+            chart.ChartAreas[0].CursorX.Interval = 0;
+            chart.ChartAreas[0].AxisX.ScaleView.Zoomable = enable;
+            chart.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            chart.ChartAreas[0].AxisX.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.SmallScroll;
+            chart.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = 0;
+
+            //chart.ChartAreas[0].CursorY.IsUserEnabled = enable;
+            //chart.ChartAreas[0].CursorY.IsUserSelectionEnabled = enable;
+            //chart.ChartAreas[0].CursorY.Interval = 0;
+            //chart.ChartAreas[0].AxisY.ScaleView.Zoomable = enable;
+            //chart.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+            //chart.ChartAreas[0].AxisY.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.SmallScroll;
+            //chart.ChartAreas[0].AxisY.ScaleView.SmallScrollMinSize = 0;
+            if (enable == false)
+            {
+                //Quita la linea de los cursores
+                chart.ChartAreas[0].CursorX.SetCursorPosition(double.NaN);
+                chart.ChartAreas[0].CursorY.SetCursorPosition(double.NaN);
+            }
+        }
+
+        private void BtnResetZoom_Click(object sender, EventArgs e)
+        {
+            //double maximo = this.chartEspectro.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+            //chartEspectro.ChartAreas[0].AxisX.Interval = (int) maximo / 10;
+            //ChartUtils.InicializarChart(chartEspectro);
+            this.chartEspectro.ChartAreas[0].AxisX.ScaleView.ZoomReset(0);
+            this.chartEspectro.ChartAreas[0].AxisY.ScaleView.ZoomReset(0);
+
+        }
+
+        private void BtnMarcadores_Click(object sender, EventArgs e)
+        {
+
+            //Marca los puntos del grafico. Todos
+            // Configurar estilo del marcador
+            //chartEspectro.Series[0].MarkerStyle = MarkerStyle.Circle;  // Estilo de marcador (ej. círculo)
+            //chartEspectro.Series[0].MarkerSize = 10;                   // Tamaño del marcador
+            //chartEspectro.Series[0].MarkerColor = Color.Red;           // Color del marcador
+
+            // Crear una línea vertical en el gráfico
+            //VerticalLineAnnotation annotation = new VerticalLineAnnotation();
+            //annotation.AxisX = chartEspectro.ChartAreas[0].AxisX;
+            //annotation.AxisY = chartEspectro.ChartAreas[0].AxisY;
+            //annotation.X = 2;  // Posición en el eje X
+            //annotation.LineColor = Color.Blue;
+            //annotation.LineWidth = 2;
+
+            //// Agregar la anotación al gráfico
+            //chartEspectro.Annotations.Add(annotation);
+
+
+            //// Crear una anotación de texto
+            // Crear una anotación de texto en el punto (X=3, Y=30)
+            TextAnnotation textAnnotation = new TextAnnotation();
+            textAnnotation.Text = "Marca en (3, 30)";
+            textAnnotation.X = 3;   // Posición en el eje X
+            textAnnotation.Y =0.23;  // Posición en el eje Y
+            textAnnotation.Font = new Font("Arial", 10, FontStyle.Bold);
+            textAnnotation.ForeColor = Color.Blue;
+
+            // Colocar la anotación en la ubicación correcta
+            textAnnotation.AnchorX = 3;
+            textAnnotation.AnchorY = 0.33;
+            textAnnotation.AnchorDataPoint = chartEspectro.Series[0].Points.FindMaxByValue();//.FindByValue(30, "Y");  // Anclar a un punto específico
+
+            // Agregar la anotación al gráfico
+            chartEspectro.Annotations.Add(textAnnotation);
+
+            // Agregar un marcador en el punto específico (X=3, Y=30)
+            DataPoint markerPoint = new DataPoint(3, 0.24);
+            markerPoint.MarkerStyle = MarkerStyle.Star4;  // Estilo del marcador (ej. estrella)
+            markerPoint.MarkerSize = 10;                 // Tamaño del marcador
+            markerPoint.MarkerColor = Color.Red;         // Color del marcador
+
+            // Agregar el marcador a la serie
+            chartEspectro.Series[0].Points.Add(markerPoint);
+        }
+
+        private void BtnLineaReferencia_Click(object sender, EventArgs e)
+        {
+            // Crear una línea horizontal en el eje Y
+            StripLine stripLine = new StripLine();
+            stripLine.IntervalOffset = 0.2;  // Valor en el eje Y donde se dibujará la línea
+            stripLine.BorderColor = Color.Red;
+            stripLine.BorderWidth = 2;
+            stripLine.BorderDashStyle = ChartDashStyle.Dash;
+
+            // Agregar la línea de referencia al eje Y
+            chartEspectro.ChartAreas[0].AxisY.StripLines.Add(stripLine);
+            chartEspectro.ChartAreas[0].AxisX.StripLines.Add(stripLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Crear una nueva serie para los puntos aislados
+            Series isolatedPointsSeries = new Series("Puntos Aislados");
+            isolatedPointsSeries.ChartType = SeriesChartType.Point;  // Tipo de gráfico: puntos
+            isolatedPointsSeries.MarkerStyle = MarkerStyle.Circle;   // Estilo de los marcadores
+            isolatedPointsSeries.MarkerSize = 8;                    // Tamaño de los marcadores
+            isolatedPointsSeries.Color = Color.Red;                 // Color de los puntos
+
+            // Agregar puntos aislados
+            isolatedPointsSeries.Points.AddXY(2, 10);  // Punto aislado en (X=2, Y=10)
+            isolatedPointsSeries.Points.AddXY(5, 15);  // Punto aislado en (X=5, Y=15)
+            isolatedPointsSeries.Points.AddXY(7, 5);   // Punto aislado en (X=7, Y=5)
+
+            // Agregar la serie al gráfico
+            chartEspectro.Series.Add(isolatedPointsSeries);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Crear una línea vertical en el punto X=3
+            VerticalLineAnnotation verticalLine = new VerticalLineAnnotation();
+            verticalLine.AxisX = chartEspectro.ChartAreas[0].AxisX;
+            verticalLine.AxisY = chartEspectro.ChartAreas[0].AxisY;
+            verticalLine.X = 3;  // Posición en el eje X
+            verticalLine.LineColor = Color.Green;
+            verticalLine.LineWidth = 2;
+            verticalLine.LineDashStyle = ChartDashStyle.Dash;
+
+            // Agregar la línea vertical al gráfico
+            chartEspectro.Annotations.Add(verticalLine);
+
+            // Agregar un marcador en la intersección de la línea vertical y el eje Y
+            Series markerSeries = new Series("Marcador");
+            markerSeries.ChartType = SeriesChartType.Point;
+            markerSeries.MarkerStyle = MarkerStyle.Circle;   // Estilo del marcador
+            markerSeries.MarkerSize = 10;                    // Tamaño del marcador
+            markerSeries.MarkerColor = Color.Red;            // Color del marcador
+
+            // Agregar el marcador en la posición X=3, Y=25 (por ejemplo)
+            markerSeries.Points.AddXY(2.5, 0.3);
+
+            // Agregar la serie con el marcador al gráfico
+            chartEspectro.Series.Add(markerSeries);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Crear una línea vertical que sea interactiva
+            VerticalLineAnnotation interactiveLine = new VerticalLineAnnotation();
+            interactiveLine.AxisX = chartEspectro.ChartAreas[0].AxisX;
+            interactiveLine.AxisY = chartEspectro.ChartAreas[0].AxisY;
+            interactiveLine.X = 3;  // Posición en el eje X
+            interactiveLine.LineColor = Color.Blue;
+            interactiveLine.LineWidth = 2;
+            interactiveLine.LineDashStyle = ChartDashStyle.Solid;
+            interactiveLine.ToolTip = "Haz clic en esta línea";  // Mostrar un tooltip al pasar el mouse
+
+            //// Agregar un evento para capturar clics
+            //interactiveLine.Click += (sender, e) => {
+            //    MessageBox.Show("¡Hiciste clic en la línea!");
+            //};
+
+            // Agregar la anotación al gráfico
+            chartEspectro.Annotations.Add(interactiveLine);
+
+        }
+
+        private void chartEspectro_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void chartEspectro_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Convertir las coordenadas del clic del mouse a elementos del gráfico
+            HitTestResult result = chartEspectro.HitTest(e.X, e.Y);
+
+            // Verificar si el clic fue en un punto de la serie
+            if (result.ChartElementType == ChartElementType.DataPoint)
+            {
+                // Obtener el punto que fue tocado
+                DataPoint clickedPoint = _graphSerie.Points[result.PointIndex];
+
+                // Mostrar un mensaje con la información del punto
+                MessageBox.Show($"¡Hiciste clic en el punto (X={clickedPoint.XValue}, Y={clickedPoint.YValues[0]})!");
+            }
+        }
+    }
+}
