998d51d4c354cb12695686230daab4b130641f1c
Raul Palavecino <raulpalav@gmail.com>
2024-10-14 01:12:26 -0300

    +Prop. BackColor al ToggleButton

--- Soft/ECGViewer/ECGViewer/Controles/ToggleButton.cs
+++ Soft/ECGViewer/ECGViewer/Controles/ToggleButton.cs
@@ -8,60 +8,41 @@ namespace ECGViewer.Controles
     public class ToggleButton : CheckBox
     {
         //Fields
-        private Color onBackColor = Color.MediumSlateBlue;
-        private Color onToggleColor = Color.WhiteSmoke;
-        private Color offBackColor = Color.Gray;
-        private Color offToggleColor = Color.Gainsboro;
-        private bool solidStyle = true;
+        private Color _onBackColor = Color.MediumSlateBlue;
+        private Color _onToggleColor = Color.WhiteSmoke;
+        private Color _offBackColor = Color.Gray;
+        private Color _offToggleColor = Color.Gainsboro;
+        private bool _solidStyle = true;
+
+        private Color backgroundColor = Color.Transparent;
 
         //Properties
         [Category("RJ Code Advance")]
         public Color OnBackColor
         {
-            get { return onBackColor; }
-
-            set
-            {
-                onBackColor = value;
-                this.Invalidate();
-            }
+            get { return _onBackColor; }
+            set{ _onBackColor = value; this.Invalidate(); }
         }
 
         [Category("RJ Code Advance")]
         public Color OnToggleColor
         {
-            get
-            { return onToggleColor; }
-
-            set
-            {
-                onToggleColor = value;
-                this.Invalidate();
-            }
+            get { return _onToggleColor; }
+            set { _onToggleColor = value; this.Invalidate(); }
         }
 
         [Category("RJ Code Advance")]
         public Color OffBackColor
         {
-            get { return offBackColor; }
-
-            set
-            {
-                offBackColor = value;
-                this.Invalidate();
-            }
+            get { return _offBackColor; }
+            set { _offBackColor = value; this.Invalidate(); }
         }
 
         [Category("RJ Code Advance")]
         public Color OffToggleColor
         {
-            get { return offToggleColor; }
-
-            set
-            {
-                offToggleColor = value;
-                this.Invalidate();
-            }
+            get { return _offToggleColor; }
+            set { _offToggleColor = value; this.Invalidate(); }
         }
 
         [Browsable(false)]
@@ -74,19 +55,22 @@ namespace ECGViewer.Controles
         [DefaultValue(true)]
         public bool SolidStyle
         {
-            get { return solidStyle; }
+            get { return _solidStyle; }
+            set { _solidStyle = value; this.Invalidate(); }
+        }
 
-            set
-            {
-                solidStyle = value;
-                this.Invalidate();
-            }
+        [Category("RJ Code Advance")]
+        public Color BackgroundColor
+        {
+            get { return backgroundColor; }
+            set { backgroundColor = value; this.Invalidate(); }
         }
 
         //Constructor
         public ToggleButton()
         {
             this.MinimumSize = new Size(45, 22);
+            this.BackColor = Color.Transparent; // Inicializa como transparente
         }
 
         //Methods
@@ -111,24 +95,34 @@ namespace ECGViewer.Controles
             pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
             pevent.Graphics.Clear(this.Parent.BackColor);
 
+            // Cambia el fondo del control a transparente o usa el color definido
+            if (backgroundColor == Color.Transparent)
+            {
+                pevent.Graphics.Clear(this.Parent.BackColor);
+            }
+            else
+            {
+                pevent.Graphics.Clear(backgroundColor);
+            }
+
             if (this.Checked) //ON
             {
                 //Draw the control surface
-                if (solidStyle)
-                    pevent.Graphics.FillPath(new SolidBrush(onBackColor), GetFigurePath());
-                else pevent.Graphics.DrawPath(new Pen(onBackColor, 2), GetFigurePath());
+                if (_solidStyle)
+                    pevent.Graphics.FillPath(new SolidBrush(_onBackColor), GetFigurePath());
+                else pevent.Graphics.DrawPath(new Pen(_onBackColor, 2), GetFigurePath());
                 //Draw the toggle
-                pevent.Graphics.FillEllipse(new SolidBrush(onToggleColor),
+                pevent.Graphics.FillEllipse(new SolidBrush(_onToggleColor),
                     new Rectangle(this.Width - this.Height + 1, 2, toggleSize, toggleSize));
             }
             else //OFF
             {
                 //Draw the control surface
-                if (solidStyle)
-                    pevent.Graphics.FillPath(new SolidBrush(offBackColor), GetFigurePath());
-                else pevent.Graphics.DrawPath(new Pen(offBackColor, 2), GetFigurePath());
+                if (_solidStyle)
+                    pevent.Graphics.FillPath(new SolidBrush(_offBackColor), GetFigurePath());
+                else pevent.Graphics.DrawPath(new Pen(_offBackColor, 2), GetFigurePath());
                 //Draw the toggle
-                pevent.Graphics.FillEllipse(new SolidBrush(offToggleColor),
+                pevent.Graphics.FillEllipse(new SolidBrush(_offToggleColor),
                     new Rectangle(2, 2, toggleSize, toggleSize));
             }
         }
